27a9d1676eef73f649577bd16619d1a49b10b9ec
James Batt <getjamesbatt@gmail.com>
2020-03-30 20:16:04 +1100

    removed doc-generator

--- internal/doc-generator/markdown.ts
+++
@@ -1,7 +0,0 @@
-export function code(value: string) {
-  return '`' + value + '`';
-}
-
-export function block(value: string, language: string) {
-  return '```' + language + '\n' + value + '\n```'
-}
