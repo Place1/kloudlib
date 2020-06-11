23af562b94bfd8e412d558a808970fa6be5635ec
James Batt <getjamesbatt@gmail.com>
2020-06-11 11:40:31 +1000

    utils remove helm tests fix

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -6,16 +6,13 @@ export function removeHelmTests() {
     if (obj.metadata && obj.metadata.annotations && obj.metadata.annotations['helm.sh/hook']) {
       // transforms in nodejs expects you to mutate input object, not return a new one.
       // https://github.com/pulumi/pulumi-kubernetes/blob/4b01b5114df3045cecefd2ff3e2f2ed64430e3dd/sdk/nodejs/yaml/yaml.ts#L2214
-      for (const key in obj) {
-        delete obj[key];
-      }
-      Object.assign(obj, {
-        kind: 'List',
-        apiVersion: 'v1',
-        metadata: {},
-        items: [],
-      });
-      return;
+      obj.apiVersion = "v1";
+      obj.kind = "List";
+      obj.items = [];
+
+      // returning null for when this is merged:
+      // https://github.com/pulumi/pulumi-kubernetes/pull/666
+      return null;
     }
     return;
   };
