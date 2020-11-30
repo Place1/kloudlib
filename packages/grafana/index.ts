e7ae3f5d9d747dc859650435f3d9a10ed727a582
James Batt <getjamesbatt@gmail.com>
2020-11-30 11:34:30 +1100

    updated grafana version

--- packages/grafana/index.ts
+++ packages/grafana/index.ts
@@ -127,7 +127,7 @@ export class Grafana extends pulumi.ComponentResource implements GrafanaOutputs 
 
     this.meta = pulumi.output<abstractions.HelmMeta>({
       chart: 'grafana',
-      version: props?.version ?? '6.1.4',
+      version: props?.version ?? '6.1.10',
       repo: 'https://grafana.github.io/helm-charts',
     });
 
